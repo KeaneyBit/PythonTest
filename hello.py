3090b469afa2b182f16908219f956232564cff5a
John Keaney <trinity.keaney@gmail.com>
2020-10-12 00:05:31 +0100

    Changed File

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print(msg)
 debugshortccut = "Press F5 to Debug."
 debugshortccut.capitalize
 print("\n" + debugshortccut + "\n")
+testingGit = "Testing my first proper Git upload."
+print(testingGit)
